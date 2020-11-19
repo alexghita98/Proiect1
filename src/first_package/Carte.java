79a2308920be64717495c08a095b42b79a5a0754
Alex Ghita <alexandru.ghita98@gmail.com>
2020-11-19 08:32:17 +0200

    Documenteaza clasa Carte
    Issue: #3
    Documentare clasa carte conform #3
    
    Signed-off-by: Alex Ghita <alexandru.ghita98@gmail.com>

--- src/first_package/Carte.java
+++ src/first_package/Carte.java
@@ -1,6 +1,18 @@
 package first_package;
 
+/**
+ * Modeleaza un obiect de tip carte
+ *
+ * <p>Cartea va face parte dintr-o biblioteca</p>
+ *
+ * @see Biblioteca
+ * @author andrei.ghita
+ */
+
 public class Carte implements Comparable<Carte>{
+    /**
+     * Titlu cartii ar trebui sa fie un string de maxim 30 de caractere
+     */
     private String titlu;
     private String autor;
     private int an_aparitie;
@@ -18,6 +30,12 @@ public class Carte implements Comparable<Carte>{
     public String toString() {
         return this.titlu + " scrisa de " + this.autor + " publicata in " + this.an_aparitie;
     }
+
+    /**
+     * Compara obiectul curent cu cel dat ca <b>parametru</b>
+     * @param o obiectul cu care se compara
+     * @return intoarce 1 daca this > o, 0 altfel
+     */
 
     @Override
     public int compareTo(Carte o) {
